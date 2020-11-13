61da75b853351013b0fcea2354a5afb9c62b00d8
RubaHashash <ruba.hashash@gmail.com>
2020-11-13 23:14:28 +0200

    todo list

---
+++ src/Todo.js
@@ -0,0 +1,163 @@
+import React, {Component} from 'react'
+import uuid from "uuid";
+
+
+class Todo extends Component{
+    
+    constructor(props) {
+        super(props)
+
+       this.input=React.createRef()
+       this.state={
+           list:[],
+           id:'',
+           value: '',
+           completed: ''
+          }
+
+          this.handleChange = this.handleChange.bind(this)
+    }
+
+    // completedStyle = {
+    //     fontStyle: "italic",
+    //     color: "#cdcdcd",
+    //     textDecoration: "line-through"
+
+    // }
+
+    handleChange(id){
+        this.setState(prevState => {
+            const updateList = prevState.list.map(todo => {
+                if(todo.id === id){
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                list: updateList
+                
+            }
+        })
+
+        localStorage.setItem("list", JSON.stringify(this.state.list))
+
+    }
+
+
+    addTask = () =>{
+        const Items={
+            id:uuid.v4(),
+            value: this.input.current.value,
+            completed: "true"
+        };
+
+        if(localStorage.getItem('list')==null){
+            const list = []
+            list.push(Items)
+            localStorage.setItem("list", JSON.stringify(list))
+        }
+        else{
+            const list = JSON.parse(localStorage.getItem('list'))
+            list.push(Items)
+            localStorage.setItem("list", JSON.stringify(list))
+        }
+        this.setState({
+            list:JSON.parse(localStorage.getItem('list'))
+        });
+    }
+
+    editTask = (id,value) =>{
+
+        const list = JSON.parse(localStorage.getItem('list'))
+        list.map(item=>{
+            if(item.id===id){
+              item.value=value;
+            }
+          })
+          this.setState({
+            list:list
+          })
+          localStorage.setItem("list", JSON.stringify(list))        
+    }
+
+
+
+componentDidMount(){
+    const List = window.localStorage.getItem('list')
+    const parseList = JSON.parse(List)
+
+    if(List ==null){
+        return false
+    }
+    else{
+        this.setState({
+            list: parseList
+        });
+    }
+}
+
+
+deleteItem = (event) =>{
+    let index = event.target.getAttribute('data-key')
+    let listValue = JSON.parse(localStorage.getItem('list'))
+    listValue.splice(index,1)
+    this.setState({list:listValue})
+    localStorage.setItem('list',JSON.stringify(listValue))
+}
+
+
+clearTask = () =>{
+
+    localStorage.clear();
+    
+    this.setState({
+        list:[],
+    });
+}
+
+
+    render() {
+        return (
+            <div className="main-container">
+                <h1>My Todo List</h1>
+                <hr/>
+                <div className="todo-item">
+                    <input className="add-input" type="text" placeholder="Insert Here" ref={this.input}></input>
+                        <button onClick={this.addTask} className="button" >+</button>
+                            <ul className="ba">
+                                {
+                                    this.state.list.map((item,index)=>
+                                    {
+                                        return(
+                                        <li key={item.id}> 
+                                        <div   className="todo-div">
+                                        <div   className="todo-div">
+                                            <input type="checkbox" onChange={(event)=>this.handleChange(item.id)}></input>
+                                            <input  className="input-view" type="text" id={item.id} value={item.value} onChange={(e)=>{this.editTask(item.id,e.target.value)}}/>
+                                            {/* <label style={!item.completed ? this.completedStyle: null}>{item.value}</label> */}
+                                        </div>
+                                        <div className="btn-div">
+                                            <button className="button" type="button" value="delete" data-key={index} onClick={this.deleteItem}>Delete</button>
+                                        </div>
+
+                                        </div>
+  
+                                        <hr/>
+
+                                        </li>)
+                                    })
+                                } 
+                            </ul>
+
+                        <div className="btn-div">
+                            <button onClick={this.clearTask} className="button" >clear</button>
+                        </div>
+
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Todo;
